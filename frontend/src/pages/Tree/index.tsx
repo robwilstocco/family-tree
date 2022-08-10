e89438f855887d471fb8212a78559fb2ed443bed
Robert Wilson <robwilstocco@gmail.com>
2022-08-09 23:42:08 -0300

    melhoria na exibição da arvore

--- frontend/src/pages/Tree/index.tsx
+++ frontend/src/pages/Tree/index.tsx
@@ -38,24 +38,28 @@ export default function Tree(){
     if(pessoa.person_name === personName && pessoa.id_relation === 1){
 
       const root = document.getElementById('tree')!;
-      const div_family = document.createElement('div');
+      const div_family = document.createElement('ul');
       div_family.className = "family"
       root.appendChild(div_family);
 
-      const div_parents = document.createElement('div');
+      const div_parents = document.createElement('li');
       div_parents.className ="parents";
-      div_parents.setAttribute('id',`${'relation_'+pessoa.id_relation}`);
       div_family.appendChild(div_parents);
 
-      const div_sons = document.createElement('div');
+      const div_persons = document.createElement('div');
+      div_persons.className ="person";
+      div_persons.setAttribute('id',`${'relation_'+pessoa.id_relation}`);
+      div_parents.appendChild(div_persons);
+
+      const div_sons = document.createElement('ul');
       div_sons.className = "sons";
       div_sons.setAttribute('id',`${'sons_'+pessoa.id_relation}`);
-      div_family.appendChild(div_sons);
+      div_parents.appendChild(div_sons);
 
       const a = document.createElement('a');
       a.setAttribute('href','/newperson');
       a.onclick = () => newPerson(pessoa);
-      div_parents.appendChild(a);
+      div_persons.appendChild(a);
 
       const h6 = document.createElement('h6');
       h6.className = "primary"
@@ -82,16 +86,21 @@ export default function Tree(){
     }
     else{
       const leaf = document.getElementById(`${'sons_'+pessoa.id_parents}`)!;
-      const div_family = document.createElement('div');
+      const div_family = document.createElement('li');
       div_family.className = "family"
       leaf.appendChild(div_family);
 
-      const div_parents = document.createElement('div');
+      const div_parents = document.createElement('li');
       div_parents.className ="parents";
-      div_parents.setAttribute('id',`${'relation_'+pessoa.id_relation}`);
-      div_family.appendChild(div_parents);
+      // div_parents.setAttribute('id',`${'relation_'+pessoa.id_relation}`);
+      // div_family.appendChild(div_parents);
 
-      const div_sons = document.createElement('div');
+      const div_persons = document.createElement('div');
+      div_persons.className ="person";
+      div_persons.setAttribute('id',`${'relation_'+pessoa.id_relation}`);
+      div_family.appendChild(div_persons);
+
+      const div_sons = document.createElement('ul');
       div_sons.className = "sons";
       div_sons.setAttribute('id',`${'sons_'+pessoa.id_relation}`);
       div_family.appendChild(div_sons);
@@ -99,12 +108,11 @@ export default function Tree(){
       const a = document.createElement('a');
       a.setAttribute('href','/newperson')
       a.onclick = () => newPerson(pessoa);
-      div_parents.appendChild(a);
+      div_persons.appendChild(a);
 
       const h6 = document.createElement('h6');
       h6.className = "primary"
       h6.append(pessoa.person_name);
-
       a.appendChild(h6);
 
       return
@@ -124,9 +132,7 @@ export default function Tree(){
     }).then(response =>{
       setPersons(response.data);
     })
-  },[treeId]);
-
-  
+  },[treeId]);  
 
   return(
     <div className="tree-container">
@@ -140,16 +146,11 @@ export default function Tree(){
           <FiPower size={18} color = "#de1581"/>
         </button>
       </header>
-      <div className="tree" id='tree'>
-
-        {persons.map(person => {
-          return(
-            <div key={person.id_person}>
-              {fill(person)}
-            </div>
-          )}
-        )
-        }
+      <div className="tree-diagram" id='tree'>
+        {persons.map((person) => {
+              return fill(person)        
+        })
+      }      
       </div>
     </div>
   );
